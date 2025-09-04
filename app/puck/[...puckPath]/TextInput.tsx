6dba307ad19ef5f83d7c17da044c1a03562a32b2
TonyzCataldo <antoniotonyzz71@gmail.com>
2025-09-03 21:38:58 -0300

    feat: TextInput flex to text or number

--- app/puck/[...puckPath]/TextInput.tsx
+++ app/puck/[...puckPath]/TextInput.tsx
@@ -1,11 +1,12 @@
-export default function TextInput({ value, name, onChange }) {
+export default function TextInput({ value, name, onChange, type }) {
   return (
     <input
-      className="bg-white  w-full placeholder:text-[var(--txt)] rounded-md border font-sans text-[var(--txt)] focus:border-green-500 border-gray-700 outline-0  p-3"
-      type="text"
+      className="bg-white  w-full placeholder:text-[var(--txt)] rounded-md border font-sans text-[var(--txt)] focus:border-green-500 border-gray-300 outline-0  p-3"
+      type={type}
       value={value ?? ""}
       placeholder={name}
       onChange={(e) => onChange(e.currentTarget.value)}
+      onWheel={(e) => e.currentTarget.blur()}
     />
   );
 }
